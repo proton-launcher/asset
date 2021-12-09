4ac253018ac2c83cf3c39755aad0ae7451b0fd72
danterusdev <danterusdev@gmail.com>
2021-12-08 19:59:30 -0800

    fix optifine

--- installation/1.8.9-optifine/install.js
+++ installation/1.8.9-optifine/install.js
@@ -1,7 +1,7 @@
 download("https://optifine.net/adloadx?f=OptiFine_1.8.9_HD_U_M5.jar", files + "/Optifine-1.8.9_HD_U_M5.html");
 let html = read(files + "/Optifine-1.8.9_HD_U_M5.html");
 let capture = regex_capture(html, "'(.*?)' onclick='onDownload()");
-download("https://optifine.net/" + capture, "OptiFine-1.8.9_HD_U_M5.jar");
+download("https://optifine.net/" + capture, files + "/OptiFine-1.8.9_HD_U_M5.jar");
 extract(files + "/OptiFine-1.8.9_HD_U_M5.jar", files + "/optifine-extract");
 
 copy_file(files + "/optifine-extract/launchwrapper-of-2.2.jar", files + "/launchwrapper-of-2.2.jar");
