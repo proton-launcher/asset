0fc291a787f7881889eb12fba4d467656d3076ba
danterusdev <danterusdev@gmail.com>
2021-12-10 19:12:11 -0800

    forge 1.8.9!

---
+++ installation/1.8.9-forge/install.js
@@ -0,0 +1,6 @@
+download("https://maven.minecraftforge.net/net/minecraftforge/forge/1.8.9-11.15.1.2318-1.8.9/forge-1.8.9-11.15.1.2318-1.8.9-universal.jar", files + "/forge-1.8.9-11.15.1.2318-1.8.9-universal.jar");
+download("https://libraries.minecraft.net/net/minecraft/launchwrapper/1.12/launchwrapper-1.12.jar", files + "/launchwrapper-1.12.jar");
+download("https://libraries.minecraft.net/org/ow2/asm/asm-all/5.0.3/asm-all-5.0.3.jar", files + "/asm-all-5.0.3.jar");
+download("https://libraries.minecraft.net/lzma/lzma/0.0.1/lzma-0.0.1.jar", files + "/lzma-0.0.1.jar");
+download("https://libraries.minecraft.net/net/sf/trove4j/trove4j/3.0.3/trove4j-3.0.3.jar", files + "/trove4j-3.0.3.jar");
+download("https://libraries.minecraft.net/java3d/vecmath/1.5.2/vecmath-1.5.2.jar", files + "/vecmath-1.5.2.jar");
---
+++ installation/1.8.9-forge/launch.js
@@ -0,0 +1,13 @@
+main_class = "net.minecraft.launchwrapper.Launch";
+
+classpath.push(files + "/forge-1.8.9-11.15.1.2318-1.8.9-universal.jar");
+classpath.push(files + "/launchwrapper-1.12.jar");
+classpath.push(files + "/asm-all-5.0.3.jar");
+classpath.push(files + "/lzma-0.0.1.jar");
+classpath.push(files + "/trove4j-3.0.3.jar");
+classpath.push(files + "/vecmath-1.5.2.jar");
+
+program_arguments.push("--tweakClass");
+program_arguments.push("net.minecraftforge.fml.common.launcher.FMLTweaker");
+
+policies.push(replace(read(files + "/policy.policy"), "{files}", files));
