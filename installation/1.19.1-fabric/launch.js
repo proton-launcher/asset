6792835bdecfecaaca1fc3eea0d4ff2b4258c79b
danterusdev <danterusdev@gmail.com>
2022-08-02 10:07:12 -0700

    1.19.1 fabric

---
+++ installation/1.19.1-fabric/launch.js
@@ -0,0 +1,13 @@
+main_class = "net.fabricmc.loader.impl.launch.knot.KnotClient"
+
+classpath.push(files + "/fabric-loader-0.14.8.jar")
+classpath.push(files + "/asm-9.3.jar")
+classpath.push(files + "/asm-tree-9.3.jar")
+classpath.push(files + "/asm-commons-9.3.jar")
+classpath.push(files + "/tiny-mappings-parse-0.3.0+build.17.jar")
+classpath.push(files + "/sponge-mixin-0.11.4+mixin.0.8.5.jar")
+classpath.push(files + "/tiny-remapper-0.8.2.jar")
+classpath.push(files + "/access-widener-2.1.0.jar")
+classpath.push(files + "/asm-analysis-9.3.jar")
+classpath.push(files + "/asm-util-9.3.jar")
+classpath.push(files + "/intermediary-1.19.1.jar")
